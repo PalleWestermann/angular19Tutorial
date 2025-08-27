fd233c398aa32507be8bef73cf0788410ebab823
Palle Westermann <pwe@tec.dk>
2025-08-27 10:52:18 +0200

    WorksNow

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { DataBinding } from "./components/data-binding/data-binding";
-import { NgIf } from "./components/ng-if/ng-if";
+import { NgIf } from './components/ng-if/ng-if';
+
 
 @Component({
   selector: 'app-root',
