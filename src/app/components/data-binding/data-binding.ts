fa913f4bf712ac671b8653d3e8db4b463955b448
Palle Westermann <pwe@tec.dk>
2025-08-27 10:04:54 +0200

    InclOneWayDataBinding

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -11,11 +11,22 @@ export class DataBinding {
   rollNo: number = 121;
   isActive: boolean = true;
   currentDate: Date = new Date();
+  myPlaceholder: string = "Enter Full Name";
+  div1ClassName:  string ="bg-primary";
 
   constructor() {
     console.log(this.firstName);
     this.isActive = false;
     console.log(this.isActive);
+  }
+
+  showWelcomeMessage() {
+    alert("Welcome to Angular 20 Tutorials")
+  }
+
+  onCityChanges() {
+    console.log("City Changed");
     
   }
+
 }
