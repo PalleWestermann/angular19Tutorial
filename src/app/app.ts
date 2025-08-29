b0dbf6d80e4a476fbfb3fe9c77c8d25db4df46eb
Palle Westermann <pwe@tec.dk>
2025-08-29 06:57:29 +0200

    NgFor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgIf } from './components/ng-if/ng-if';
+import { NgFor } from "./components/ng-for/ng-for";
 
 
 @Component({
   selector: 'app-root',
-  imports: [NgIf],
+  imports: [NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/ng-for/ng-for.ts
+++ src/app/components/ng-for/ng-for.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-for.html',
   styleUrl: './ng-for.css'
 })
 export class NgFor {
+  cityList: string[] = ["Pune","Nagpur","Jaipur","Mumbai","Thane"];
 
+  employeeArray: any[] = [
+    {empId: 121, name:'AAA',city:'Pune',contactNo:'999889988'},
+    {empId: 122, name:'BBB',city:'Nagpur',contactNo:'11111111'},
+    {empId: 123, name:'ccc',city:'Jaipur',contactNo:'22222222'},
+    {empId: 124, name:'DDD',city:'Pune',contactNo:'33333333'},
+    {empId: 125, name:'EEE',city:'Thane',contactNo:'44444'},
+  ]
 }
